117c158e1a00f0bc43818ffeb9390136e124c7e9
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-10-22 13:05:52 -0300

    test: refactor tests and add benchmark

--- blog-renderer_test.go
+++ blog-renderer_test.go
@@ -3,6 +3,7 @@ package blogrenderer_test
 import (
 	"bytes"
 	blogrenderer "go-blog"
+	"io"
 	"testing"
 
 	approvals "github.com/approvals/go-approval-tests"
@@ -16,14 +17,34 @@ func TestRender(t *testing.T) {
 		Tags:        []string{"dev", "go", "tdd"},
 	}
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
 
-		if err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	postRenderer, _ := blogrenderer.NewPostRenderer()
+
+	aPost := blogrenderer.Post{
+		Title:       "hello, world!",
+		Body:        "this is my first post",
+		Description: "This is a description",
+		Tags:        []string{"dev", "go", "tdd"},
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
