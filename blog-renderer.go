2f32809af85791d97040e902019b724f4d3b3cef
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-10-27 09:28:05 -0300

    feat: make the posts index

--- blog-renderer.go
+++ blog-renderer.go
@@ -5,6 +5,7 @@ import (
 	"embed"
 	"fmt"
 	"io"
+	"strings"
 	"text/template"
 
 	"github.com/yuin/goldmark"
@@ -31,13 +32,31 @@ func NewPostRenderer() (*PostRenderer, error) {
 func (r PostRenderer) Render(w io.Writer, post Post) error {
 	var bodyMdToHtml bytes.Buffer
 	if err := goldmark.Convert([]byte(post.Body), &bodyMdToHtml); err != nil {
-
 		return fmt.Errorf("error on converting body to html: %q", err.Error())
 	}
 
 	post.Body = bodyMdToHtml.String()
 
 	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", post); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{titleToSlug .Title}}">{{.Title}}</a></li>{{end}}</ol>`
+	templ, err := template.New("index").Funcs(template.FuncMap{
+		"titleToSlug": func(title string) string {
+			return strings.ToLower(strings.ReplaceAll(title, " ", "-"))
+		},
+	}).Parse(indexTemplate)
+
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, posts); err != nil {
 		return err
 	}
 
